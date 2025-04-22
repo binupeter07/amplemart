4c469081ffa4492c3c3c23479c93e3302ec1c089
Binu Peter <binupeterp07@gmail.com>
2025-04-23 00:17:29 +0100

    Final

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,6 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ urlencoded: true , extended:true }));
-app.use;
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://amplemart.vercel.app"],
